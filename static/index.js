e53038b7f09832af1e10067bbd14b54a83f32fc3
Neil Grogan <neil@grogan.ie>
2019-09-13 11:03:25 +0000

    Starting movie ratings

--- static/index.js
+++ static/index.js
@@ -1,7 +1,11 @@
-function create_card(id, title, img_src) {
-    const div = document.createElement('div');
-    div.setAttribute("class", 'card');
-    div.setAttribute("id", 'card' + id);
+function create_image(src, div){
+    const img = document.createElement('img');
+    img.src = src;
+    div.appendChild(img);
+}
+
+function create_header(id, div, s){
+    const title = s['title'];
 
     const header = document.createElement('div');
     header.setAttribute("class", 'card__header');
@@ -10,20 +14,59 @@ function create_card(id, title, img_src) {
     theader.textContent = title;
     header.appendChild(theader);
 
-    const main = document.createElement('div');
-    main.setAttribute("class", 'card__main');
-    main.setAttribute("id", 'card__main' + id);
-
-    const img = document.createElement('img');
-    img.src = img_src;
-    main.appendChild(img);
-
     div.appendChild(header);
-    div.appendChild(main);
+}
+
+function create_left_side(id, div, s){
+    const left = document.createElement('div');
+    left.setAttribute("class", 'card__left');
+    left.setAttribute("id", 'card__left' + id);
+
+    
+    create_image(s['image'], left);
+
+    div.appendChild(left);
+}
+
+function create_ratings(div, s){
+    const ratings = s['ratings'];
+    for(rating in ratings){
+        console.log(ratings[rating]);
+    }
+}
+
+function create_description(div, s){
+    const description = s['description'];
+    const desc = document.createElement('p');
+    desc.innerHTML = description;
+    desc.setAttribute("class", 'card__description');
+    div.appendChild(desc);
+
+}
+
+function create_right_side(id, div, s){ 
+    const right = document.createElement('div');
+    right.setAttribute("class", 'card__right');
+    right.setAttribute("id", 'card__right' + id);
+
+    create_ratings(right, s);
+    create_description(right, s);
+    div.appendChild(right);
+}
+
+
+function create_card(s) {
+    const id = s['id'];
+
+    const div = document.createElement('div');
+    div.setAttribute("class", 'card');
+    div.setAttribute("id", 'card' + id);
+
+    create_header(id, div, s);
+    create_left_side(id, div, s);
+    create_right_side(id, div, s);
 
     document.body.appendChild(div);
-
-    return main;
 }
 
 window.onload = function () {
@@ -32,7 +75,7 @@ window.onload = function () {
         .then(data => {
             let id = 0;
             for (let s of data) {
-                create_card(s['id'], s['title'], s['image']);
+                create_card(s);
             }
         });
 };
